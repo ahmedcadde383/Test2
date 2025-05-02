f4d846fdffdfc23fe473bfbacbc5bb65704c5225
AhmedNaafic <ahmedmahad699@gmail.com>
2025-05-02 17:27:11 +0300

    Test2 Project

---
+++ src/test/java/com/test/demoTest2/DemoTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.demoTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
